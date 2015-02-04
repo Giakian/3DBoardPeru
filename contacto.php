f586267650b3461d22398f9b151d54d7019bf0d5
Marcos <sockramx@gmail.com>
2015-02-04 01:16:16 -0500

    formulario

--- contacto.php
+++ contacto.php
@@ -11,15 +11,13 @@
 			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1950.2526682003995!2d-76.99450360945532!3d-12.145960478565632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b8114c37afeb%3A0xfa7f47af4b7f649a!2sJuan+Soto+Bermeo%2C+Santiago+de+Surco!5e0!3m2!1ses!2spe!4v1418584682762" frameborder="0" style="border:0"></iframe>
 		</div>
 		<article class="form_cont">
-			<form class="miformulario">
-				<label for='nombre'>*Nombre:</label>
-				<input type='text' id='nombre' class="campo" placeholder='Escribe aqui tu Nombre' required />
-				<label for='correo'>*Edad:</label>
-				<input type='number' id='correo' class="campo" min="0" max="100" required/>
-				<label for='correo'>*E-mail:</label>
-				<input type='email' id='correo' class="campo" placeholder='Escriba aqui su e-mail' required/>
-				<label for='asunto'>*Asunto:</label>
-				<input type='text' id='asunto' class="campo" required/>
+			<form class="miformulario" action="enviar.php" method="POST">
+				<label>*Nombre:</label>
+				<input type='text' id='nombre' name="nombre" class="campo" placeholder='Escribe aqui tu Nombre' required />
+				<label>*E-mail:</label>
+				<input type='text' id='asunto' name="asunto" class="campo" required/>
+				<label>*Asunto:</label>
+				<textarea name="asunto" id="" name="email" cols="30" rows="10" placeholder='Escriba aqui su e-mail' required></textarea>
 				<input type="submit" id="btnenviar" value="Enviar Datos" class="boton" />
 			</form>
 		</article>
---
+++ enviar.php
@@ -0,0 +1,13 @@
+<?php 
+
+echo "llegando datos";
+
+$nombre=$_POST['nombre'];
+$email=$_POST['email'];
+$asunto=$_POST['asunto'];
+
+
+echo $nombre;
+echo $email;
+echo $asunto;
+ ?>
